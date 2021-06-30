fe808065f0ba14e5a238b47a6ae0de23d1d9a839
Joker <joker_jiejie@163.com>
2021-06-30 18:10:32 +0800

    初始化

---
+++ drone-test1/src/main/java/com/example/dronetest1/DroneTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.dronetest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTest1Application.class, args);
+    }
+
+}
---
+++ drone-test1/src/test/java/com/example/dronetest1/DroneTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronetest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
